14cdc0041f56a259318210fb8e7a81dde378d1fa
Randy Russell <randyhr2000@gmail.com>
2021-09-10 11:53:31 -0700

    first commit

---
+++ src/FirstRobot/RobotPlayer.java
@@ -0,0 +1,180 @@
+package FirstRobot;
+import battlecode.common.*;
+
+import java.util.Random;
+
+public strictfp class RobotPlayer {
+    static RobotController rc;
+
+    static final RobotType[] spawnableRobot = {
+        RobotType.POLITICIAN,
+        RobotType.SLANDERER,
+        RobotType.MUCKRAKER,
+    };
+
+    static final Direction[] directions = {
+        Direction.NORTH,
+        Direction.NORTHEAST,
+        Direction.EAST,
+        Direction.SOUTHEAST,
+        Direction.SOUTH,
+        Direction.SOUTHWEST,
+        Direction.WEST,
+        Direction.NORTHWEST,
+    };
+
+    static final int EC_MIN_BID = 2;
+    static final double EC_HIGHER_BID_CHANCE_PERCENTAGE = 0.2;
+    static final double EC_HIGHER_BID_SPEND_PERCENTAGE = 0.7;
+
+    enum politicianMode
+    {
+        DEFEND,
+        GATHER,
+        ATTACK
+    }
+
+    /**
+     * run() is the method that is called when a robot is instantiated in the Battlecode world.
+     * If this method returns, the robot dies!
+     **/
+    @SuppressWarnings("unused")
+    public static void run(RobotController rc) throws GameActionException {
+
+        // This is the RobotController object. You use it to perform actions from this robot,
+        // and to get information on its current status.
+        RobotPlayer.rc = rc;
+
+
+        System.out.println("I'm a " + rc.getType() + " and I just got created!");
+        while (true) {
+            // Try/catch blocks stop unhandled exceptions, which cause your robot to freeze
+            try {
+
+                // Here, we've separated the controls into a different method for each RobotType.
+                // You may rewrite this into your own control structure if you wish.
+                System.out.println("I'm a " + rc.getType() + "! Location " + rc.getLocation());
+                switch (rc.getType()) {
+                    case ENLIGHTENMENT_CENTER: runEnlightenmentCenter(); break;
+                    case POLITICIAN:           runPolitician();          break;
+                    case SLANDERER:            runSlanderer();           break;
+                    case MUCKRAKER:            runMuckraker();           break;
+                }
+
+                // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
+                Clock.yield();
+
+            } catch (Exception e) {
+                System.out.println(rc.getType() + " Exception");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static void runEnlightenmentCenter() throws GameActionException {
+
+        int toBid;
+
+        Random r = new Random();
+        float chance = r.nextFloat();
+        if (chance <= EC_HIGHER_BID_CHANCE_PERCENTAGE)
+            toBid = (int)Math.floor(rc.getInfluence() * EC_HIGHER_BID_SPEND_PERCENTAGE);
+        else
+            toBid = EC_MIN_BID;
+
+        RobotType toBuild;
+
+
+
+
+
+        int influence = 50;
+        for (Direction dir : directions) {
+            //if (rc.canBuildRobot(toBuild, dir, influence)) {
+            //    rc.buildRobot(toBuild, dir, influence);
+            //} else {
+            //    break;
+            //}
+        }
+    }
+
+    static MapLocation[] GetPoliticianDefencePositionsRelativeToEC()
+    {
+        MapLocation ECLocation = rc.getLocation();
+        int x = ECLocation.x;
+        int y = ECLocation.y;
+
+        return new MapLocation[] {
+
+        };
+    }
+
+    static void runPolitician() throws GameActionException {
+        Team enemy = rc.getTeam().opponent();
+        int actionRadius = rc.getType().actionRadiusSquared;
+        RobotInfo[] attackable = rc.senseNearbyRobots(actionRadius, enemy);
+        if (attackable.length != 0 && rc.canEmpower(actionRadius)) {
+            System.out.println("empowering...");
+            rc.empower(actionRadius);
+            System.out.println("empowered");
+            return;
+        }
+        if (tryMove(randomDirection()))
+            System.out.println("I moved!");
+    }
+
+    static void runSlanderer() throws GameActionException {
+        if (tryMove(randomDirection()))
+            System.out.println("I moved!");
+    }
+
+    static void runMuckraker() throws GameActionException {
+        Team enemy = rc.getTeam().opponent();
+        int actionRadius = rc.getType().actionRadiusSquared;
+        for (RobotInfo robot : rc.senseNearbyRobots(actionRadius, enemy)) {
+            if (robot.type.canBeExposed()) {
+                // It's a slanderer... go get them!
+                if (rc.canExpose(robot.location)) {
+                    System.out.println("e x p o s e d");
+                    rc.expose(robot.location);
+                    return;
+                }
+            }
+        }
+        if (tryMove(randomDirection()))
+            System.out.println("I moved!");
+    }
+
+    /**
+     * Returns a random Direction.
+     *
+     * @return a random Direction
+     */
+    static Direction randomDirection() {
+        return directions[(int) (Math.random() * directions.length)];
+    }
+
+    /**
+     * Returns a random spawnable RobotType
+     *
+     * @return a random RobotType
+     */
+    static RobotType randomSpawnableRobotType() {
+        return spawnableRobot[(int) (Math.random() * spawnableRobot.length)];
+    }
+
+    /**
+     * Attempts to move in a given direction.
+     *
+     * @param dir The intended direction of movement
+     * @return true if a move was performed
+     * @throws GameActionException
+     */
+    static boolean tryMove(Direction dir) throws GameActionException {
+        System.out.println("I am trying to move " + dir + "; " + rc.isReady() + " " + rc.getCooldownTurns() + " " + rc.canMove(dir));
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+            return true;
+        } else return false;
+    }
+}
